8501ea599d30ae7a7398b7fc04ca47499e97b56e
masoodiocod <masood@iocod.com>
2025-05-19 17:52:44 +0530

    feat: add dashboard card components and update dashboard layout

---
+++ app/components/cards/dash-sec-one-cards/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps<{
+  image: string
+  title: string
+  value: string | number
+}>()
+</script>
+
+<template>
+  <div class="card-main p-[12px] rounded-xl bg-white shadow-md">
+    <div class="flex lg:flex-col lg:items-left xl:flex-col xl:items-left 2xl:flex-row  2xl:items-center gap-2">
+      <div class="w-[26px] h-[26px]">
+        <img :src="image" alt="" class="w-full h-full object-contain">
+      </div>
+      <div>
+        <h2 class="text-bases font-normal">
+          {{ title }}
+        </h2>
+      </div>
+    </div>
+    <div class="mt-2">
+      <h1 class="text-2xl font-extrabold">
+        {{ value }}
+      </h1>
+    </div>
+  </div>
+</template>
