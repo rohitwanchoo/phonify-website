056e02112757694c822b88dc3c90c22217502a21
Fahadk-c <fahad@iocod.com>
2025-05-20 15:23:51 +0530

    Navbar improvements. Improved File Structure.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
-import AppSidebar from '@/components/AppSidebar.vue'
 import DashSecOne from '@/components/dash-components/dash-sec-one/index.vue'
 import DashSecTwo from '@/components/dash-components/dash-sec-two/index.vue'
-
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -11,26 +9,22 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb'
+
 import { Separator } from '@/components/ui/separator'
 import {
   SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
+  SidebarProvider
 } from '@/components/ui/sidebar'
+import MainBar from '~/components/app-sidebar/Main.vue'
 </script>
 
 <template>
   <SidebarProvider>
-    <AppSidebar />
+    <MainBar />
     <SidebarInset>
       <header class="flex h-16  items-center gap-2 ">
         <div class="w-full">
           <CoreNavbar />
-          <!-- <SidebarTrigger class="-ml-1 md:hidden" /> -->
-          <!-- <Separator
-            orientation="vertical"
-            class="mr-2 data-[orientation=vertical]:h-4"
-          /> -->
           <!-- <Breadcrumb>
             <BreadcrumbList>
               <BreadcrumbItem class="hidden md:block">
@@ -46,10 +40,8 @@ import {
           </Breadcrumb> -->
         </div>
       </header>
-      <div class="flex flex-1 flex-col gap-4 p-4 pt-5 md:rounded-tl-xl bg-red-900">
-        <DashSecOne />
-        <DashSecTwo />
-        <div class="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+      <div class="flex flex-1 flex-col gap-4 p-4 pt-5 md:rounded-tl-xl bg-white">
+        <slot />
       </div>
     </SidebarInset>
   </SidebarProvider>
---
+++ app/pages/dashboard/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import DashSecOne from '@/components/dash-components/dash-sec-one/index.vue'
+import DashSecTwo from '@/components/dash-components/dash-sec-two/index.vue'
+</script>
+
+<template>
+  <div class="flex flex-1 flex-col gap-4 p-4 pt-5 md:rounded-tl-xl bg-white">
+    <DashSecOne />
+    <DashSecTwo />
+    <div class="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+  </div>
+</template>
