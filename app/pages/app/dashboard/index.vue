430fabdac14e280225e51a1b19dd6101ba3de0f5
masoodiocod <masood@iocod.com>
2025-05-20 10:57:05 +0530

    feat: add DashSecTwo component to dashboard layout

--- app/pages/app/dashboard/index.vue
+++ app/pages/app/dashboard/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import AppSidebar from '@/components/AppSidebar.vue'
 import DashSecOne from '@/components/dash-components/dash-sec-one/index.vue'
+import DashSecTwo from '@/components/dash-components/dash-sec-two/index.vue'
 
 import {
   Breadcrumb,
@@ -47,6 +48,7 @@ import {
       </header>
       <div class="flex flex-1 flex-col gap-4 p-4 pt-5 md:rounded-tl-xl bg-red-900">
         <DashSecOne />
+        <DashSecTwo />
         <div class="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
       </div>
     </SidebarInset>
