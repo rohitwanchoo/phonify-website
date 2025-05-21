b79f7532f1cf313363f387a345f8d1486d600393
basilrayeestp <basilrayees@iocod.com>
2025-05-21 21:17:29 +0530

    feat(campaign): add campaign management components and pages
    This commit introduces new components and pages for managing campaigns, including creation, preview, and selection functionalities. It also includes updates to UI components like buttons, sheets, and switches to support the new features. The changes enhance the application's ability to handle campaign-related operations efficiently.

---
+++ app/components/base/Header.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb'
+
+defineProps<HeaderProps>()
+
+interface HeaderProps {
+  title?: string
+  breadcrumbs?: {
+    label: string
+    href?: string
+    active?: boolean
+  }[]
+}
+</script>
+
+<template>
+  <div class="flex items-center justify-between">
+    <div class="text-2xl font-medium space-y-2">
+      <div v-if="breadcrumbs?.length">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <div v-for="(item, index) in breadcrumbs" :key="item.label" class="flex items-center gap-x-2">
+              <BreadcrumbItem>
+                <BreadcrumbLink :href="item.href" class="font-normal" :class="item.active && 'text-primary'">
+                  {{ item.label }}
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator v-if="index !== breadcrumbs.length - 1" />
+            </div>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+      <div>
+        {{ title }}
+      </div>
+    </div>
+    <div class="flex items-center gap-x-3">
+      <slot name="actions" />
+    </div>
+  </div>
+</template>
---
+++ app/components/campaign/create.vue
@@ -0,0 +1,3 @@
+<template>
+  hello from create campaign
+</template>
