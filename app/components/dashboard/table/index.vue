481434e41a587c02ee68542e24c218ad2f620357
masoodiocod <masood@iocod.com>
2025-06-12 15:59:11 +0530

    style: adjust padding and font sizes across various components for improved layout

--- app/components/dashboard/table/index.vue
+++ app/components/dashboard/table/index.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="grid grid-cols-1 md:grid-cols-7 gap-6">
-        <div class="col-span-5">
+    <div class="grid grid-cols-1 lg:grid-cols-7 gap-6">
+        <div class="col-span-6 lg:col-span-5">
             <dashboardTableCallCount />
         </div>
-        <div class="col-span-2 w-full">
+        <div class="col-span-6 lg:col-span-2 w-full">
             <dashboardTableLiveCall />
         </div>
         
