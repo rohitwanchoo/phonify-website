4bcdf8e21ef3471306ea900db487423213abcdb8
FAHAD K C <fahad@iocod.com>
2025-06-13 10:49:27 +0530

    feat(lead-management): add lead management table and search functionality

--- app/pages/app/lead-management/list.vue
+++ app/pages/app/lead-management/list.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+import { Button } from '~/components/ui/button'
+import { Input } from '~/components/ui/input'
+</script>
+
 <template>
-    list
+  <BaseHeader title="Lists">
+    <template #actions>
+      <div class="relative">
+        <Input placeholder="Search List" />
+        <Icon class="absolute top-[9px] right-2" name="lucide:search" />
+      </div>
+      <Nuxt-link to="/app/campaign/new-campaign">
+        <Button>
+          <Icon class="!text-white" name="lucide:plus" />
+          Create Lists
+        </Button>
+      </Nuxt-link>
+    </template>
+  </BaseHeader>
+  <!-- TABLE -->
+  <div>
+    <LeadManagementListsTable />
+  </div>
 </template>
