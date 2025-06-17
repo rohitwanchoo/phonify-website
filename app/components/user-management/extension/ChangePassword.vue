ad88bbccc7f31416d8036a897f02e44cc1d7ba39
basilrayeestp <basilrayees@iocod.com>
2025-06-17 16:26:20 +0530

    fix: change DialogClose button type from submit to button

--- app/components/user-management/extension/ChangePassword.vue
+++ app/components/user-management/extension/ChangePassword.vue
@@ -83,7 +83,7 @@ const onSubmit = handleSubmit((values) => {
 
         <DialogFooter class="mt-4">
           <DialogClose class="w-[49%]">
-            <Button variant="outline" class="w-full h-11" type="submit" form="dialogForm">
+            <Button variant="outline" class="w-full h-11" type="button" form="dialogForm">
               <Icon name="material-symbols:close" />
               Close
             </Button>
