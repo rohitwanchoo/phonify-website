b9c2907df9c33870a3eea036a1b50a67554f1e4d
FAHAD K C <fahad@iocod.com>
2025-06-25 16:49:25 +0530

    feat(voip-configuration): add AddButton, EditDialog, and Table components for VoIP configuration management

---
+++ app/components/configuration/voip-configuration/AddButton.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Button } from '~/components/ui/button'
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '~/components/ui/dialog'
+
+const dialogOpen = ref(false)
+</script>
+
+<template>
+  <Dialog v-model:open="dialogOpen">
+    <Button @click="dialogOpen = true">
+      <Icon class="!text-white" name="lucide:plus" />
+      Add VoIP Configuration
+    </Button>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Add VoIP Configuration</DialogTitle>
+      </DialogHeader>
+      <div class="py-6 text-center text-gray-500">
+        Dummy UI for VoIP Configuration Dialog
+      </div>
+      <DialogFooter>
+        <Button @click="dialogOpen = false">
+          Close
+        </Button>
+      </DialogFooter>
+    </DialogContent>
+  </Dialog>
+</template>
---
+++ app/components/configuration/voip-configuration/EditVoipConfigurationDialog.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { Button } from '~/components/ui/button'
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '~/components/ui/dialog'
+
+const props = defineProps<{
+  open: boolean
+  row: any
+}>()
+const emit = defineEmits(['update:open'])
+
+const dialogOpen = computed({
+  get: () => props.open,
+  set: val => emit('update:open', val),
+})
+</script>
+
+<template>
+  <Dialog v-model:open="dialogOpen">
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Edit VoIP Configuration</DialogTitle>
+      </DialogHeader>
+      <div class="py-6 text-center text-gray-500">
+        Dummy UI for editing VoIP configuration.<br>
+        <span v-if="row">Editing: <b>{{ row.name }}</b></span>
+      </div>
+      <DialogFooter>
+        <Button @click="dialogOpen = false">
+          Close
+        </Button>
+      </DialogFooter>
+    </DialogContent>
+  </Dialog>
+</template>
