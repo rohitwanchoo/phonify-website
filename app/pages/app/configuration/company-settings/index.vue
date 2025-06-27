50399b1cc21532b2d25b04ab4c9e6f95238f8eee
masoodiocod <masood@iocod.com>
2025-06-27 10:24:41 +0530

    feat(api): add Company Settings page and remove Notification Settings page

---
+++ app/pages/app/configuration/company-settings/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Input } from '~/components/ui/input'
+</script>
+
+<template>
+  <BaseHeader title="Company Settings" />
+  <div>
+    <div class="grid grid-cols-1 md:grid-cols-12 gap-4 mb-4">
+      <div class="bg-white border rounded-lg p-4 md:col-span-7">
+    <ConfigurationCompanySettingsSystemEmailSettings/>
+      </div>
+      <div class="bg-white border rounded-lg p-4 md:col-span-5">
+        <!-- Second column content -->
+        Column 2
+      </div>
+    </div>
+    <div class="bg-white border rounded-lg p-4">
+      <!-- Full width row content -->
+      Full width row
+    </div>
+  </div>
+</template>
