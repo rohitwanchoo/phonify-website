e9be7f06e5873a39eb65a2be7288921aef8f34c3
masoodiocod <masood@iocod.com>
2025-06-30 17:43:44 +0530

    created smtp section

---
+++ app/components/configuration/smtp-settings/ActionDropDown.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+const emits = defineEmits(['delete', 'duplicate'])
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="ghost" size="icon">
+        <Icon name="lucide:ellipsis-vertical" size="20" />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent>
+      <DropdownMenuGroup>
+        <DropdownMenuItem class="cursor-pointer hover:!text-primary" @click="emits('duplicate')">
+          <Icon name="material-symbols:content-copy-outline" size="16" />
+          Duplicate
+        </DropdownMenuItem>
+        <DropdownMenuItem class="text-red-600 cursor-pointer hover:!text-red-500" @click="emits('delete')">
+          <Icon name="mdi:trash-can" size="16" />
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuGroup>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
---
+++ app/components/configuration/smtp-settings/Verified.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import approveImage from '~/assets/svg/approve.svg'
+import { Button } from '~/components/ui/button'
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '~/components/ui/dialog'
+
+
+
+defineEmits(['close'])
+
+const open = defineModel<boolean>('open', { default: false })
+
+
+</script>
+
+<template>
+  <Dialog v-model:open="open">
+    <DialogContent class="bg-[url('/images/dialog-background/decline-bg.jpg')] bg-cover bg-center">
+      <div>
+        <img class="h-[98px] w-[98px] mx-auto mt-10" :src="approveImage" alt="decline Image">
+        <div class="text-center mt-4">
+          <p class="text-xl font-semibold text-primary">
+            Configuration Setting Verified
+          </p>
+          <p class="text-sm text-primary mb-5 mt-1">
+            You Configuration setting has been verified, you can continue
+          </p>
+        </div>
+      </div>
+      <div class="flex gap-2">
+        <Button variant="outline" class="w-full h-10 bg-[#162D3A] text-white hover:bg-[#162D3A] hover:text-white" @click="open = false">
+          <Icon name="material-symbols:close" size="18" />
+          Close
+        </Button>
+      </div>
+    </DialogContent>
+  </Dialog>
+</template>
