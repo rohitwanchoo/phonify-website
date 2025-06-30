296f023499fa7d6d6a7d9440b7855a6f3ad59d5d
FAHAD K C <fahad@iocod.com>
2025-06-30 12:38:46 +0530

    Created allowed ips page and table ui.

--- app/pages/app/configuration/allowed-ips.vue
+++ app/pages/app/configuration/allowed-ips.vue
@@ -1,5 +1,29 @@
+<script setup lang="ts">
+import { Button } from '~/components/ui/button'
+import { Input } from '~/components/ui/input'
+</script>
+
 <template>
+  <div class="">
+    <!-- HEADER -->
+    <BaseHeader title="Allowed IPs">
+      <template #actions>
+        <div class="relative">
+          <Input placeholder="Search List" />
+          <Icon class="absolute top-[9px] right-2" name="lucide:search" />
+        </div>
+        <Nuxt-link to="/app/campaign/new-campaign">
+          <Button>
+            <Icon class="!text-white" name="lucide:plus" />
+            Add IPs
+          </Button>
+        </Nuxt-link>
+      </template>
+    </BaseHeader>
+
+    <!-- TABLE -->
     <div>
-        allowed ips
+      <ConfigurationAllowedIpsTable />
     </div>
+  </div>
 </template>
