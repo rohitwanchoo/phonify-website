c96a7d4b3b9c74fb54bca225bca414ecf9689762
FAHAD K C <fahad@iocod.com>
2025-07-21 11:35:33 +0530

    Addedtext Template section and related table

--- app/pages/app/sms/text-template.vue
+++ app/pages/app/sms/text-template.vue
@@ -1,5 +1,27 @@
+<script setup lang="ts">
+import { Button } from '~/components/ui/button'
+import { Input } from '~/components/ui/input'
+</script>
+
 <template>
+  <div class="">
+    <!-- HEADER -->
+    <BaseHeader title="SMS Template List">
+      <template #actions>
+        <div class="relative">
+          <Input placeholder="Search List" />
+          <Icon class="absolute top-[9px] right-2" name="lucide:search" />
+        </div>
+        <Button>
+          <Icon class="!text-white" name="lucide:plus" />
+          Add SMS Template
+        </Button>
+      </template>
+    </BaseHeader>
+
+    <!-- TABLE -->
     <div>
-        
+      <SmsTextTemplateTable />
     </div>
+  </div>
 </template>
