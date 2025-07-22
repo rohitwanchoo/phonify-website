15a5a7695e4f3ed7d92710b3d2c03091e775b808
FAHAD K C <fahad@iocod.com>
2025-07-22 16:27:14 +0530

    call data reports ui completed.

--- app/pages/app/report/call-data-reports.vue
+++ app/pages/app/report/call-data-reports.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-    hello call data reports
+  <div>
+    <!-- HEADER -->
+    <BaseHeader title="Call Reports">
+      <template #actions>
+      
+
+        <!-- Trigger Button -->
+       <ReportCallDataFilterSheet class="bg-black text-white" v-model:form-values="filterValues" @apply-filter="applyFilter" />
+      </template>
+    </BaseHeader>
+
+    <!-- TABLE -->
+    <div>
+      <ReportIvrLogsTable />
+    </div>
+  </div>
 </template>
