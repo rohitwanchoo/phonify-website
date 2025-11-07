5c32cac1107efbc8e2421f8c705b7873d0afd655
Fathima Afeefa <afeefa@iocod.com>
2025-11-07 11:30:03 +0530

    feat:updated app-intro section

--- app/components/public/sections/home/app-intro/index.vue
+++ app/components/public/sections/home/app-intro/index.vue
@@ -1,3 +1,24 @@
 <template>
-    app intro
+    <div class="w-full bg-white px-4 md:px-28 py-8 md:py-24">
+        <div class="max-w-[1440px] mx-auto w-full  flex flex-col text-center items-center gap-16">
+            <div class=" flex flex-col gap-4">
+                <p class="text-stone-800 text-base font-light">
+                    <span
+                        class="inline-block w-3 h-3 bg-[#D11E28] [clip-path:polygon(25%_6.7%,75%_6.7%,100%_50%,75%_93.3%,25%_93.3%,0%_50%)]" />
+                    App Intro
+                </p>
+                <h1 class="text-stone-900 text-2xl md:text-5xl font-medium leading-9">
+                    Phonify Mobile App
+                </h1>
+                <p>Take your call center with you!
+                    Because work doesn’t have to stop when you leave your desk.
+                </p>
+            </div>
+            <div>
+                <img src="/assets/svg/public/footer/app-store.svg" alt="">
+                
+            </div>
+        </div>
+    </div>
+
 </template>
