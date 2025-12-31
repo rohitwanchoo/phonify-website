82c3d02d649023c940ebf172d23249845f8fb8ac
basilrayeestp <basilrayees@iocod.com>
2025-12-31 15:06:55 +0530

    style: remove unnecessary empty class attributes

--- app/components/campaign/TimeBaseCalling.vue
+++ app/components/campaign/TimeBaseCalling.vue
@@ -98,9 +98,9 @@ function onEdit() {
 
 <template>
   <div class="border rounded-lg bg-white">
-    <div class=" ">
+    <div>
       <Accordion v-model="accordion" collapsible>
-        <AccordionItem value="item-1" class="">
+        <AccordionItem value="item-1">
           <div class="flex items-center justify-between px-5 h-[57px]" :class="accordion === 'item-1' && 'border-b'">
             <div class="text-[16px] font-medium text-primary/100">
               Time Based Calling
@@ -142,8 +142,8 @@ function onEdit() {
             </div>
           </div>
           <AccordionContent class="p-5">
-            <Accordion v-model="accordion2" collapsible class="">
-              <AccordionItem v-show="enableEdit" value="item-2" class="">
+            <Accordion v-model="accordion2" collapsible>
+              <AccordionItem v-show="enableEdit" value="item-2">
                 <FormField v-slot="{ errorMessage }" v-model="formState.call_schedule_id" name="call_schedule_id">
                   <FormItem v-auto-animate>
                     <FormControl>
@@ -184,7 +184,7 @@ function onEdit() {
                   </Command>
                 </AccordionContent>
               </AccordionItem>
-              <AccordionItem v-if="!enableEdit" value="item-3" class="">
+              <AccordionItem v-if="!enableEdit" value="item-3">
                 <div class="grid grid-cols-2">
                   <div class="space-y-2">
                     <div class="text-sm font-normal text-gray-400">
